e2a37f40a066912883b245a40514aab6d6c5bd86
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-05-25 07:39:50 -0700

    Adding dictionary source

---
+++ 06_dictionary_hash_table/dictionary.py
@@ -0,0 +1,17 @@
+# Objects and Dictionaries
+
+
+class Animal
+    def __init__(self, species):
+        self.species = species
+
+bear = Animal("BEAR")
+
+
+
+
+# Create an empty dictionary
+animals = []
+
+bear = {"species": "BEAR"}
+
