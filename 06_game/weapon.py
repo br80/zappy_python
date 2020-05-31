1374da126b1e7c81b1172b4641e3f1ddded2affd
Brady Fukumoto <bradyfukumoto@gmail.com>
2020-05-31 10:01:03 -0700

    Add weapon notes

--- 06_game/weapon.py
+++ 06_game/weapon.py
@@ -3,23 +3,29 @@ from object import GameObject
 
 class Weapon(GameObject):
     def __init__(self, game_id, player_row, player_col, facing, lifetime, size, game):
-        # TODO: Inherit
         self.game_id = game_id
         self.icon = game.graphics.get_icon(game_id)
         self.type = "WEAPON"
         self.game = game
         self.alive = True
 
-        row_col = self.set_row_col(player_row, player_col, facing)
-        if row_col and size > 0:
-            game.weapons.append(self)
+        row_col_valid = self.set_row_col(player_row, player_col, facing)
+
+        if row_col_valid and size > 0:
+
+            # Kill any enemies where the weapon has spawned
             for enemy in game.enemies:
                 if enemy.row == self.row and enemy.col == self.col:
                     enemy.die()
+
+            # Weapons cannot spawn on barriers or treasures
             if game.grid[self.row][self.col] != "  " and game.grid[self.row][self.col].type in ["BARRIER", "TREASURE"]:
                 if self in self.game.weapons:
                    self.game.weapons.remove(self)
+
+            # Otherwise, spawn the weapon
             else:
+                game.weapons.append(self)
                 game.grid[self.row][self.col] = self
                 self.frame_to_destroy = game.frame + lifetime
                 game.draw_screen()
